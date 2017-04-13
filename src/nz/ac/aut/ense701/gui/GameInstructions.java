aee227884586056e74524331643b9d4eefa1c2d7
James Higgins <Panda@lulzboat>
2017-04-13 15:53:25 +1200

    Updated GameInstructions

--- src/nz/ac/aut/ense701/gui/GameInstructions.java
+++ src/nz/ac/aut/ense701/gui/GameInstructions.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nz.ac.aut.ense701.gui;
 
 import java.io.BufferedReader;
@@ -17,26 +12,34 @@ import javax.swing.JOptionPane;
  * @author James Higgins
  */
 public class GameInstructions {
-    
+    /**
+     * Static method which creates a JOptionPane pop-up box containing the games instructions
+     */
     public static void instructions(){
         
-       String str = parseInstructionFile("instructions/gameinstructions.txt");
-        
+        String str = parseInstructionFile("instructions/gameinstructions.txt");   
         JOptionPane.showMessageDialog(null, str, "Game Instructions", JOptionPane.PLAIN_MESSAGE); 
-        
+     
     }
-    
+    /**
+     * Reads a text file line by line, each line is then added to a string, also formats the text
+     * into an HTML format.
+     * 
+     * @param file takes a String of where the file containing game instructions is located
+     * @return a string containing the games instructions formatted with HTML   
+    */
     public static String parseInstructionFile(String file){
         String output = "";
         try
         {
+            //Add html tags and the title for the popup box
             output = "<html><body><ul><p style=\"color:#FF0000\" ;>Game Instructions</p><br>";
-           // Open the file
+            // Open the file
             FileInputStream fileStream = new FileInputStream(file);
             BufferedReader reader = new BufferedReader(new InputStreamReader(fileStream));
 
             String line;
-            //Read File Line By Line
+            //Read File Line By Line, and add HTML formatting
             while ((line = reader.readLine()) != null)   {
               output += "<li>"+line+"</li><br>";
 
@@ -52,7 +55,7 @@ public class GameInstructions {
         }
         catch(IOException e)
         {
-            System.err.println("Problem encountered processing file.");
+            System.err.println("Could not process file.");
         }
         return output;
     }
