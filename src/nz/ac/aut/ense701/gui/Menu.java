1f130c7ada432df250b43c43511655a846f24429
wjm2202 <wjm2202@aut.ac.nz>
2017-05-18 12:39:12 +1200

    Change to dispose to remove all uneeded resources from set visable true

--- src/nz/ac/aut/ense701/gui/Menu.java
+++ src/nz/ac/aut/ense701/gui/Menu.java
@@ -74,7 +74,7 @@ public class Menu extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        this.setVisible(false);
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
